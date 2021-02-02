4dadfdd1af097e6828c853f5d5f3b08b470efdf7
miglio000 <gabrielemiglio23@gmail.com>
2021-02-02 16:23:57 +0100

    Add project files.

---
+++ Array2N/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using LibreriaArray;
+namespace Array2N
+{
+    class Program
+    {
+        const int dimensione = 5;
+        static void Main(string[] args)
+        {
+            int[] vettore1 = new int[dimensione];
+            int[] vettore2 = new int[dimensione];
+            int[] vettore3 = new int[dimensione * 2];
+            Console.WriteLine("Inserisci elementi del primo vettore: ");
+            Gestione.LeggiArry(vettore1);
+            Console.WriteLine("Inserisci elementi del secondo vettore: ");
+            Gestione.LeggiArry(vettore2);
+            for(int i = 0, j = 0, k = 0; i < vettore3.Length; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    vettore3[i] = vettore2[j];
+                    j++;
+                }
+                else
+                {
+                    vettore3[i] = vettore1[k];
+                    k++;
+                }
+            }
+            Console.WriteLine("Gli elementi del vettore 3 sono: ");
+            Gestione.StampaArray(vettore3);
+        }
+    }
+}
---
+++ LibreriaArray/Gestione.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibreriaArray
+{
+    public class Gestione
+    {
+        public static void LeggiArry(int[] array)
+        {
+            for (int i = 0; i < array.Length && array[i] >= 0; i++)
+            {
+                Console.WriteLine($"Inserisci {i + 1}° elemento: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }   
+        }
+        public static void StampaArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
